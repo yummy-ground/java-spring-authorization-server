e56dffef0e77991039e5c7b3ac36bb3132031aa9
yummygyudon <bang2brew@gmail.com>
2024-10-14 01:19:57 +0900

    first commit

---
+++ src/main/kotlin/yummygyudon/java_example/sso/SsoApplication.kt
@@ -0,0 +1,11 @@
+package yummygyudon.java_example.sso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsoApplication>(*args)
+}
---
+++ src/test/kotlin/yummygyudon/java_example/sso/SsoApplicationTests.kt
@@ -0,0 +1,13 @@
+package yummygyudon.java_example.sso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
